9b858a7df660ae2aa89125c60fac9b07f8179ffe
GFZA <garfield.sirapob@hotmail.com>
2026-07-31 15:33:23 +0700

    Reviewed FIX 10 + extra
    Move the added line a bit.
    As for the extra stuff, yeah I am very biased.

--- sicko-calc-replacement/Autoloads/Resources/Trait/groove.gd
+++ sicko-calc-replacement/Autoloads/Resources/Trait/groove.gd
@@ -5,6 +5,10 @@ extends Trait
 func get_attack_mult(attacker : Beastie, _defender : Beastie, _attack : Attack, \
 					 attacker_team_controller : TeamController = null,\
 					 _defender_team_controller : TeamController = null) -> float: # Overwrite
+	var attacker_attack : Attack = attacker.my_plays[0]
+	if attacker_attack and not attacker_attack.type == Plays.Type.ATTACK_BODY:
+		return 1.0
+
 	if need_to_be_manually_activated:
 		if manually_activated:
 			return damage_dealt_mult
@@ -13,10 +17,6 @@ func get_attack_mult(attacker : Beastie, _defender : Beastie, _attack : Attack, 
 	var attacker_trait : Trait = attacker.my_trait
 	var is_groove : bool = (attacker_trait.name.to_lower() == "groove")
 	if not is_groove:
-		return 1.0
-
-	var attacker_attack : Attack = attacker.my_plays[0]
-	if attacker_attack and not attacker_attack.type == Plays.Type.ATTACK_BODY:
 		return 1.0
 
 	if not attacker_team_controller:
