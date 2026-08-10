3f91002ed1436657008e7f714d08074761bcb258
GFZA <garfield.sirapob@hotmail.com>
2026-08-10 20:15:54 +0700

    FIX: Spiker not boosting calc-from-front attacks

--- sicko-calc-replacement/Autoloads/Resources/Trait/spiker.gd
+++ sicko-calc-replacement/Autoloads/Resources/Trait/spiker.gd
@@ -2,9 +2,9 @@
 extends Trait
 
 
-func get_attack_mult(attacker : Beastie, _defender : Beastie, _attack : Attack, \
+func get_attack_mult(attacker : Beastie, _defender : Beastie, attack : Attack, \
 					 _attacker_team_controller : TeamController = null,\
 					 _defender_team_controller : TeamController = null) -> float: # Overwrite
-	if attacker.check_if_net():
+	if attacker.check_if_net() or attack.name.to_lower() in DamageCalculator.ALWAYS_CALC_FROM_FRONT_ATTACKS:
 		return damage_dealt_mult
 	return 1.0
